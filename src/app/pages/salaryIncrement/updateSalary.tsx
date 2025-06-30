70f84265a47c0e68798c7ca0fa6639ff53db5278
mikwithinnovwayz <ibrahim@innovwayz.com>
2025-06-30 20:56:30 +0300

    testDeploy9

--- src/app/pages/salaryIncrement/updateSalary.tsx
+++ src/app/pages/salaryIncrement/updateSalary.tsx
@@ -127,7 +127,7 @@ const formatNumber = (val: string | number | undefined) =>
               <th>Pay_Date</th>
               <th>Basic Allowance</th>
               <th>HR Allowance</th>
-              {/* <th>End of Service Allowance</th> The final deployment should work inshallah */}
+              {/* <th>End of Service Allowance</th> */}
               <th>Travel Allowance</th>
               <th>Salary Advance</th>
               <th>OverTime</th>
