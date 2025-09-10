e0ca99435455c8666a583f75d15040a91ee44fca
MOHAMMADIBRAHIMKHAN <ik2853884@gmail.com>
2025-09-10 12:32:04 +0300

    FixReload4

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: "appDashboard-blue",
       script: "npm",
-      args: "start",
+      args: "run dev",
       cwd: "/root/iwtDashboard/IOTADashboard-blue",
       env: { NODE_ENV: "production" }
     },
     {
       name: "appDashboard-green",
       script: "npm",
-      args: "start", 
+      args: "start dev", 
       cwd: "/root/iwtDashboard/IOTADashboard-green",
       env: {  NODE_ENV: "production" }
     }
